1be8d82ead317f8f53ab16d760cba282428908c7
mfritze <mfritze@ualberta.ca>
2014-07-14 14:35:13 -0600

    Fixed admin bug, extra $ restricted access

--- game_survey/urls.py
+++ game_survey/urls.py
@@ -8,7 +8,7 @@ from .views import ContactView, AcoreView
 
 urlpatterns = patterns('',
     #url(r'^$', HomePageView.as_view(), name='home'),
-    url(r'^admin/$', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^contact/$', ContactView.as_view(), name='Contact page'),
     url(r'^acore/$', AcoreView.as_view(), name='Acore page'),
     url(r'^surveys/', include('surveys.urls', namespace='surveys')),
